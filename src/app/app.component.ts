e0c48c069a41ae0430484f8a4218f3f7acc6f0d4
Sam David <sfd1987@gmail.com>
2017-06-26 18:06:11 -0700

    Starts expense component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{stringTest}} details!</h2>`
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Expense Tracker';
+  stringTest = 'HELLO';
+  expenses = [];
 }
---
+++ src/app/components/expenses/expense.component.ts
@@ -0,0 +1,9 @@
+export class Expense {
+  id: number;
+  description: string;
+  amount: number;
+  transactionDate: Date;
+
+  // template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+}

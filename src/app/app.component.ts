5a4103b502796c5778a674022d06faaee3cc9b3a
Sam David <sfd1987@gmail.com>
2017-06-26 20:58:05 -0700

    Home page started, home route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {Angular2TokenService} from "angular2-token";
+import {environment} from "../environments/environment";
 import { ExpenseComponent } from './components/expenses/expense.component';
+
 const EXPENSES: ExpenseComponent[] = [
   { id: 11, description: 'Mr. Nice', amount: 20 },
   { id: 11, description: 'Mr. Nice', amount: 21 },
   { id: 11, description: 'Mr. Nice', amount: 23 },
   { id: 11, description: 'Mr. Nice', amount: 53 }
 ];
-
 
 @Component({
   selector: 'app-root',
@@ -19,4 +21,22 @@ export class AppComponent {
   title = 'Expense Tracker';
   stringTest = 'HELLO';
   expenses = EXPENSES;
+
+  constructor(private authToken: Angular2TokenService){
+    this.authToken.init(environment.token_auth_config);
+
+    this.authToken.signIn({email: "sfd1987@gmail.com", password: "test123"}).subscribe(
+
+        res => {
+
+          console.log('auth response:', res);
+          console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
+          console.log('auth response body:', res.json()); //log the response body to show the user
+        },
+
+        err => {
+          console.error('auth error:', err);
+        }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
 // Components
 import {
   ExpenseComponent
 } from './components';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpenseComponent
+    ExpenseComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
